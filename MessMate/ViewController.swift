fd2b255a3ea850d433d55df5aec5a2f8c7a3a7db
Mishu <divyanshmishu2004+code@gmail.com>
2025-02-08 14:47:10 +0530

    Initial Commit

---
+++ MessMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessMate
+//
+//  Created by Divyansh Singhal on 08/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MessMateTests/MessMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  MessMateTests.swift
+//  MessMateTests
+//
+//  Created by Divyansh Singhal on 08/02/25.
+//
+
+import Testing
+@testable import MessMate
+
+struct MessMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
